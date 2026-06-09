dad3b28d69f476041f30280c64028c4dc190bf3a
Mayank6200 <mayankrajranchi1122@gmail.com>
2026-06-09 15:24:35 +0530

    Update my-script.sh

--- .github/workflows/my-script.sh
+++ .github/workflows/my-script.sh
@@ -2,5 +2,5 @@ echo "My first github workflow command"
 ls
 cat index.html
 sudo apt install cowsay -y
-cowsay -f dragon "I am a dragon and I breadth fire." >> dragon.txt
+cowsay -f dragon "I am a dragon and I breath fire." >> dragon.txt
 cat dragon.txt
